b14e911321311115a033054ecafaafbf82bfdfa4
Dillon Hafer <dh@dillonhafer.com>
2013-08-12 13:44:06 -0400

    Added admin to users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 JohnsoniteCom::Application.routes.draw do
-  devise_for :users, :path => "auth", :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+  devise_for :users, :path => "sessions", :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
