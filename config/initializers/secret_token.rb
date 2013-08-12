a3e2311443d4cebdfe8e7022a486c165aab15761
Dillon Hafer <dh@dillonhafer.com>
2013-08-12 12:08:17 -0400

    First! :P

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JohnsoniteCom::Application.config.secret_key_base = '8d4403edbc6436928b8fe0ff82645872db3e65d938cf70de7dcd53d0a593f20d5e17169798052a5c936261cb437831a787eeb693fd22970f374af034307a83b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JohnsoniteCom::Application.config.session_store :cookie_store, key: '_johnsonite_com_session'
