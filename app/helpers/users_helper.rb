c749426c6bdd7cb22c96dd0345bda926b338b501
Dillon Hafer <dh@dillonhafer.com>
2013-08-12 15:25:07 -0400

    Updated admin buttons.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,9 @@
 module UsersHelper
 
   def modify_admin_link(user)
-    if user.admin?
+    if user == current_user
+      "This is you!"
+    elsif user.admin?
       link_to 'remove admin', remove_admin_path(user), class: 'small red button radius'
     else
       link_to 'make admin', make_admin_path(user), class: 'small yellow button radius'
