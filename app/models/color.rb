8f4bb6fb9462ee0ed0d1ef0a4452da3cd1a7c3ab
Dillon Hafer <dh@dillonhafer.com>
2013-08-14 12:01:26 -0400

    Test

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,4 +1,3 @@
-include ActionView::Helpers
 class Color < ActiveRecord::Base
   belongs_to :color_palette
   validates_presence_of :color_palette_id, :color_number, :name
@@ -6,11 +5,11 @@ class Color < ActiveRecord::Base
   def image
     case color_palette.name.downcase
     when "stone"
-      asset_path "/assets/wallbase/stone/#{color_number}.jpg"
+      #asset_path "/assets/wallbase/stone/#{color_number}.jpg"
     when "wood"
-      asset_path "/assets/wallbase/wood/#{color_number}.jpg"
+      #asset_path "/assets/wallbase/wood/#{color_number}.jpg"
     when "wall art"
-      asset_path "/assets/wallbase/wallart/#{color_number}.jpg"
+      #asset_path "/assets/wallbase/wallart/#{color_number}.jpg"
     end
   end
 end
