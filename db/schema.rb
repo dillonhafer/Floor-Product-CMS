460d4c2866cfe89f2a888f6cb1fdea85469caa70
Dillon Hafer <dh@dillonhafer.com>
2013-08-15 14:20:17 -0400

    Added color to products.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130815135019) do
+ActiveRecord::Schema.define(version: 20130815181637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20130815135019) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "product_warranty_id"
+    t.integer  "color_id"
   end
 
   create_table "products_colors", id: false, force: true do |t|
